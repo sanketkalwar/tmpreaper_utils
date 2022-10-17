7f47ccbaa5311a7a05584bb8dafb29211c0591fc
rrc-trash <k.sanket10000@gmail.com>
2022-10-18 00:36:11 +0530

    Major update

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,2 +1,2 @@
-sudo apt-get update
-sudo apt-get -y install tmpreaper
+wget http://ftp.jp.debian.org/debian/pool/main/t/tmpreaper/tmpreaper_1.6.17_amd64.deb
+sudo dpkg -i tmpreaper_1.6.17_amd64.deb
--- run_tmpreaper.sh
+++ run_tmpreaper.sh
@@ -1,29 +1,29 @@
 #!/usr/bin/bash
 
 # Load config params or periodic cleanup of scratch dir
-. rrc_scratch_reaper.conf
+. scratch_reaper.conf
 
-TMPREAPER_HEAD="tmpreaper $DURATION"
+TMPREAPER_HEAD="tmpreaper --all $DURATION"
 while IFS=$'\n' read -a config
 do
   TMPREAPER_PROTECT+=' --protect '$config' '
 done < mask_directory.conf
 
-# Load all the directories from config to mask
-. mask_directory.conf
-
 echo '@@@@@@@@@@@@@@@@@@@@@@Config@@@@@@@@@@@@@@@@@@@@@@@@'
 echo 'DURATION: '$DURATION
 echo 'TARGET PATH: '$TARGET_PATH
-echo 'PROTECT PATH: '$TMPREAPER_PATH
+echo 'PROTECT PATH: '$TMPREAPER_PROTECT
 echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
 
 SEC_NANO_SEC=$(($(date +%s%N)/1000000))
 
-#access values
-echo "$PROTECT_STRING"
 
-TMPREAPER_TAIL="-t $TARGET_PATH >> 'logs/'$SEC_NANO_SEC'.log'"
+# Uncomment this for simulating the deletion of the files and folders
+#TMPREAPER_TEST_TAIL="-t $TARGET_PATH >> 'logs/'$SEC_NANO_SEC'.log'"
+#cmd=$TMPREAPER_HEAD$TMPREAPER_PROTECT$TMPREAPER_TEST_TAIL
+#eval $cmd
 
+# Uncomment this for simulating the deletion of the files and folders
+TMPREAPER_TAIL=" $TARGET_PATH >> 'logs/'$SEC_NANO_SEC'.log'"
 cmd=$TMPREAPER_HEAD$TMPREAPER_PROTECT$TMPREAPER_TAIL
 eval $cmd
